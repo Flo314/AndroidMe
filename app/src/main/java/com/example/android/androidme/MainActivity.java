f7b1201e3d753d91dd367aeb61a1475b39ffb1dc
“Florent <f.losada@hotmail.fr>
2019-02-22 14:43:11 +0100

    créer un nouveau fragment que si il n'y en a aucun sauvegardés

--- app/src/main/java/com/example/android/androidme/MainActivity.java
+++ app/src/main/java/com/example/android/androidme/MainActivity.java
@@ -13,31 +13,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // créer une instance de BodyPartFragment et l'afficher en utilisant le FragmentManager
-        BodyPartFragment headFragment = new BodyPartFragment();
+        // créer un nouveau fragment que si il n'y en a aucun sauvegardés
+        if (savedInstanceState == null){
 
-        headFragment.setImageIds(AndroidImageAssets.getHeads());
-        headFragment.setmListIndex(1);
+            // créer une instance de BodyPartFragment et l'afficher en utilisant le FragmentManager
+            BodyPartFragment headFragment = new BodyPartFragment();
 
-        // utilise le FragmentManager pour ajouter le fragment à l'écran
-        // getSupportManager donne un gestionnaire de fragment qui utilise les API de la bibliothèque de support
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        // Fragment transaction : ajoute le fragment dans le container
-        // beginTransaction démarre une nouvelle transaction
-        fragmentManager.beginTransaction()
-                .add(R.id.head_container, headFragment)
-                .commit();
+            headFragment.setImageIds(AndroidImageAssets.getHeads());
+            headFragment.setmListIndex(1);
 
-        BodyPartFragment bodyFragment = new BodyPartFragment();
-        bodyFragment.setImageIds(AndroidImageAssets.getBodies());
-        fragmentManager.beginTransaction()
-                .add(R.id.body_container, bodyFragment)
-                .commit();
+            // utilise le FragmentManager pour ajouter le fragment à l'écran
+            // getSupportManager donne un gestionnaire de fragment qui utilise les API de la bibliothèque de support
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            // Fragment transaction : ajoute le fragment dans le container
+            // beginTransaction démarre une nouvelle transaction
+            fragmentManager.beginTransaction()
+                    .add(R.id.head_container, headFragment)
+                    .commit();
 
-        BodyPartFragment legFragment = new BodyPartFragment();
-        legFragment.setImageIds(AndroidImageAssets.getLegs());
-        fragmentManager.beginTransaction()
-                .add(R.id.leg_container, legFragment)
-                .commit();
+            BodyPartFragment bodyFragment = new BodyPartFragment();
+            bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            fragmentManager.beginTransaction()
+                    .add(R.id.body_container, bodyFragment)
+                    .commit();
+
+            BodyPartFragment legFragment = new BodyPartFragment();
+            legFragment.setImageIds(AndroidImageAssets.getLegs());
+            fragmentManager.beginTransaction()
+                    .add(R.id.leg_container, legFragment)
+                    .commit();
+
+        }
     }
 }
