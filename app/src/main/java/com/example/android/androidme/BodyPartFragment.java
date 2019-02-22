55ef5c3233f7fff315370326d3554543fad7380e
“Florent <f.losada@hotmail.fr>
2019-02-22 16:32:09 +0100

    affichage de toutes les images + mise en page du fragment

--- app/src/main/java/com/example/android/androidme/BodyPartFragment.java
+++ app/src/main/java/com/example/android/androidme/BodyPartFragment.java
@@ -1,7 +1,6 @@
 package com.example.android.androidme;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
--- app/src/main/java/com/example/android/androidme/MasterListFragment.java
+++ app/src/main/java/com/example/android/androidme/MasterListFragment.java
@@ -1,6 +1,42 @@
 package com.example.android.androidme;
 
 import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.androidme.data.AndroidImageAssets;
 
 public class MasterListFragment extends Fragment {
+
+    // contructeur vide pour instancier le fragment
+    public MasterListFragment(){}
+
+    /**
+     * Gonfle le fragment et définit n'importe quelle ressource d'image
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // gonfler le fragment
+        View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+
+        // obtenir une référence à la vue de l'image
+       GridView gridView = (GridView) rootView.findViewById(R.id.image_grid_view);
+
+        //créer un adapteur
+        // afficher toutes les images
+        MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+
+        //mettre l'adapteur dans le gridView
+        gridView.setAdapter(mAdapter);
+
+        // retourne la vue racine
+        return rootView;
+    }
 }
