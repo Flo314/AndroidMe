7f6e30e329e1f021c456bdca10e05efc7385c7c0
“Florent <f.losada@hotmail.fr>
2019-02-22 08:28:31 +0100

    afficher fragment : fragment manager et transaction

--- app/src/main/java/com/example/android/androidme/MainActivity.java
+++ app/src/main/java/com/example/android/androidme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.androidme;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // créer une instance de BodyPartFragment et l'affocher en utilisant le FragmentManager
+        BodyPartFragment headFragment = new BodyPartFragment();
+
+        // utilise le FragmentManager pour ajouter le fragment à l'écran
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        // Fragment transaction : ajoute le fragment dans le container
+        fragmentManager.beginTransaction()
+                .add(R.id.head_container, headFragment)
+                .commit();
     }
 }
